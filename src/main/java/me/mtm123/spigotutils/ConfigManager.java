5ff8474784bb6d502fee8a8ad80db3247499e362
MTM123 <matiss234@gmail.com>
2018-05-23 22:35:40 +0300

    ConfigManager fix

--- src/main/java/me/mtm123/spigotutils/ConfigManager.java
+++ src/main/java/me/mtm123/spigotutils/ConfigManager.java
@@ -1,6 +1,6 @@
 package me.mtm123.factionsaddons.data;
 
-import me.mtm123.factionsaddons.FactionsAddons;
+import org.bukkit.plugin.Plugin;
 import org.bukkit.configuration.InvalidConfigurationException;
 import org.bukkit.configuration.file.FileConfiguration;
 import org.bukkit.configuration.file.YamlConfiguration;
@@ -11,18 +11,29 @@ import java.io.InputStream;
 
 public final class ConfigManager {
 
+    private static Plugin plugin = null;
+
+    public static void initialize(Plugin plugin){
+        ConfigManager.plugin = plugin;
+    }
+
     public static FileConfiguration load(String filename){
 
-        File file = new File(FactionsAddons.getInstance().getDataFolder(), filename);
+        if(plugin == null){
+            throw new IllegalStateException("ConfigManager must be initialized!");
+        }
+
+
+        File file = new File(plugin.getDataFolder(), filename);
         FileConfiguration cfg = YamlConfiguration.loadConfiguration(file);
 
         try {
             if(!file.exists()){
 
-                InputStream in = FactionsAddons.getInstance().getResource(filename);
+                InputStream in = plugin.getResource(filename);
 
                 if(in != null){
-                    FactionsAddons.getInstance().saveResource(filename, false);
+                    plugin.saveResource(filename, false);
                 }else {
                     cfg.save(file);
                 }
@@ -42,7 +53,11 @@ public final class ConfigManager {
 
     public static void save(FileConfiguration config, String filename){
 
-        File file = new File(FactionsAddons.getInstance().getDataFolder(), filename);
+        if(plugin == null){
+            throw new IllegalStateException("ConfigManager must be initialized!");
+        }
+
+        File file = new File(plugin.getDataFolder(), filename);
 
         try {
             config.save(file);
